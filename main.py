b3e49b93d3e71ced252b888be93ffdf2b41980c0
Steve Nguyen <154491875+majesticmist@users.noreply.github.com>
2024-02-29 07:50:05 +0000

    Final Solution

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 num = input('Enter a number (decimal only): ')
 # type your code here
 
-
+count = num.find('.')
+new_num = num[count+1:]
+dp = len(new_num)
 
 # do not change any code beyond this point
 print('The number', num, 'has', dp, 'decimal places.')
